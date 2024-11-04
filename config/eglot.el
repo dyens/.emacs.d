e6883c7d4ca6a617c80dcb7617364badca9d741d
Kapustin Aleksandr <alkapustin@rockitsoft.ru>
2024-11-04 23:58:59 +0300

    Add eglot file

---
+++ config/eglot.el
@@ -0,0 +1,7 @@
+;; need install manually
+;; (package-vc-install "https://github.com/jdtsmith/eglot-booster")))
+;; and compile and move to /usr/local/bin: https://github.com/blahgeek/emacs-lsp-booster
+(use-package eglot-booster
+         :ensure nil
+         :autoload eglot-booster-mode
+         :init (eglot-booster-mode 1))
