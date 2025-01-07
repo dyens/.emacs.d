efc418777158aa243e38eed8cd5d9e82667d75f1
Kapustin Aleksandr <alkapustin@rockitsoft.ru>
2025-01-07 17:11:10 +0300

    Add elfeed. TODO mv to file

--- init.el
+++ init.el
@@ -23,3 +23,19 @@
 (require 'dy-python)
 (require 'dy-tools)
 (require 'dy-macros)
+
+
+  (use-package elfeed
+    :ensure t
+    :custom
+    (elfeed-db-directory
+     (expand-file-name "elfeed" user-emacs-directory))
+    (elfeed-show-entry-switch 'display-buffer)
+    (elfeed-feeds '(
+                    ("https://www.reddit.com/r/linux.rss" reddit linux)
+                    ("https://habr.com/ru/rss/flows/develop/articles/?fl=ru" harbor)
+                    ("https://www.linux.org.ru/section-rss.jsp?section=1" linux lor)
+                    ("https://www.linux.org.ru/section-rss.jsp?section=3" linux lor)
+                    ))
+
+)
